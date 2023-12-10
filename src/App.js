7fbf33d84bee37f9f189c2b7ab39dafa0c386284
Shantanu <shantanusaini7000@gmail.com>
2023-12-10 11:41:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // State to store the calculator expression
+  const [expression, setExpression] = useState('');
+
+  // Function to handle button clicks and update the expression
+  const handleBtnClick = (value) => {
+    setExpression((prevExpression) => prevExpression + value);
+  }
+
+  // Function to handle the "AC" button click and clear the expression
+  const handleAcBtnClick = () => {
+    setExpression('');
+  }
+
+  // Function to handle the "⬅" button click and remove the last character from the expression
+  const handleBackspace = () => {
+    setExpression((prevExpression) => prevExpression.slice(0, -1));
+  }
+
+  // Function to handle the "=" button click, evaluate the expression, and update the result
+  const handleEval = () => {
+    try {
+      const result = eval(expression);
+      setExpression(result.toString());
+    } catch (error) {
+      setExpression('Error');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* Calculator UI */}
+      <div id="main">
+        <h1>Calculator</h1>
+        <div className='calc'>
+          {/* Input field to display the expression */}
+          <input type="text" name="value" id="value" value={expression} readOnly />
+          {/* Calculator buttons */}
+          <div className="btns">
+            <div>
+              {/* Clear button */}
+              <button onClick={() => handleAcBtnClick()}>AC</button>
+              {/* Backspace button */}
+              <button onClick={() => handleBackspace()}>⬅</button>
+              {/* Evaluate button */}
+              <button onClick={() => handleEval()}>=</button>
+              {/* Division button */}
+              <button onClick={() => handleBtnClick('/')}>/</button>
+            </div>
+            <div>
+              {/* Number buttons */}
+              <button onClick={() => handleBtnClick('7')}>7</button>
+              <button onClick={() => handleBtnClick('8')}>8</button>
+              <button onClick={() => handleBtnClick('9')}>9</button>
+              {/* Addition button */}
+              <button onClick={() => handleBtnClick('+')}>+</button>
+            </div>
+            <div>
+              {/* Number buttons */}
+              <button onClick={() => handleBtnClick('6')}>6</button>
+              <button onClick={() => handleBtnClick('5')}>5</button>
+              <button onClick={() => handleBtnClick('4')}>4</button>
+              {/* Subtraction button */}
+              <button onClick={() => handleBtnClick('-')}>-</button>
+            </div>
+            <div>
+              {/* Number buttons */}
+              <button onClick={() => handleBtnClick('3')}>3</button>
+              <button onClick={() => handleBtnClick('2')}>2</button>
+              <button onClick={() => handleBtnClick('1')}>1</button>
+              {/* Multiplication button */}
+              <button onClick={() => handleBtnClick('*')}>*</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
